897622b2b10dee105ae29137cf4e9bbd6133c773
Outrigger047-HOME <andrew.wyshak@gmail.com>
2015-12-15 10:53:14 -0500

    Add Cell.cs

---
+++ GameOfLife/GameOfLife/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public enum CellStateTypes
+    {
+        Dead = 0x0,
+        Alive = 0x1
+    }
+
+    public class Cell
+    {
+        public CellStateTypes State { get; private set; }
+        // what
+    }
+}
