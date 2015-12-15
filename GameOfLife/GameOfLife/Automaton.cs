608215c7e3bce69a037f68f31999b5a382300944
Outrigger047-HOME <andrew.wyshak@gmail.com>
2015-12-15 14:13:36 -0500

    Organized Automaton class structure into regions. Incremental stopping point.

--- GameOfLife/GameOfLife/Automaton.cs
+++ GameOfLife/GameOfLife/Automaton.cs
@@ -5,11 +5,24 @@ namespace GameOfLife
 {
     public class Automaton
     {
-        // Fields
+        #region Fields
+        /// <summary>
+        /// Stores the current state of the universe
+        /// </summary>
         private Cell[,] universe;
+        /// <summary>
+        /// Dimensions of the universe
+        /// </summary>
         private int sizeX, sizeY;
+        #endregion
 
-        // Constructor
+        #region Constructor
+        /// <summary>
+        /// Parameterized constructor to instantiate Automaton and initialize member fields and properties
+        /// </summary>
+        /// <param name="sizeXIn">Horizontal dimension of the universe</param>
+        /// <param name="sizeYIn">Vertical dimension of the universe</param>
+        /// <param name="initLiveCells">List of coordinates indicating which cells are alive initially</param>
         public Automaton(int sizeXIn, int sizeYIn, List<CoordSet> initLiveCells)
         {
             // Set size of universe
@@ -32,22 +45,54 @@ namespace GameOfLife
             // Initialize age of universe
             Age = 0;
         }
+        #endregion
 
-        // Enums
+        #region Enums
+        /// <summary>
+        /// Used for neighbor-finding
+        /// </summary>
         enum Cardinals
         {
             N, NE, E, SE, S, SW, W, NW
         }
+        #endregion
 
-        // Properties
+        #region Properties
+        /// <summary>
+        /// Auto-implemented property to store, track, and return the current age of the universe
+        /// </summary>
         public int Age { get; private set; }
+
+        /// <summary>
+        /// Returns horizontal dimension of the universe
+        /// </summary>
         public int SizeX { get { return sizeX; } }
+        /// <summary>
+        /// Returns vertical dimension of the universe
+        /// </summary>
         public int SizeY { get { return sizeY; } }
 
-        // Methods
+        /// <summary>
+        /// Returns a list of live cells in the universe
+        /// </summary>
+        public List<CoordSet> Universe
+        {
+            get
+            {
+                List<CoordSet> uOut = new List<CoordSet>();
+
+                // TODO Implement universe conversion to list of live cells
+
+                return uOut;
+            }
+        }
+        #endregion
+
+        #region Methods
+        #endregion
 
 
-        // Nested classes
+        #region Nested classes
         class Cell
         {
             public CellStateTypes State { get; set; }
@@ -58,6 +103,7 @@ namespace GameOfLife
                 Alive = 0x1
             }
         }
+        #endregion
     }
 
     public struct CoordSet
--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace GameOfLife
 {
@@ -8,6 +9,7 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
+            #region Get console input
             Console.Write("Horiz size of universe: ");
             int sizeX = Convert.ToInt32(Console.ReadLine());
 
@@ -21,8 +23,8 @@ namespace GameOfLife
             Console.WriteLine("1   External text file");
             Console.WriteLine("2   Manual list input");
             List<CoordSet> liveCells = new List<CoordSet>();
-            ConsoleKeyInfo keyPress = Console.ReadKey();
-            switch (keyPress.ToString())
+            ConsoleKeyInfo inputModeKeyPress = Console.ReadKey();
+            switch (inputModeKeyPress.ToString())
             {
                 case "1":
                     string[] fileData;
@@ -31,6 +33,16 @@ namespace GameOfLife
                     try
                     {
                         fileData = File.ReadAllLines(Console.ReadLine());
+                        foreach (var line in fileData)
+                        {
+                            if (Regex.IsMatch(line, "^[0-9]+,[0-9]+$"))
+                            {
+                                liveCells.Add(
+                                    new CoordSet(
+                                        Convert.ToInt32(line.Split(',')[0]), 
+                                        Convert.ToInt32(line.Split(',')[1])));
+                            }
+                        }
                     }
                     catch (IOException ioe)
                     {
@@ -41,14 +53,29 @@ namespace GameOfLife
                         throw;
                     }
 
-
-
                     break;
                 case "2":
+                    ConsoleKeyInfo moreCoordsKeyPress;
+                    do
+                    {
+                        Console.Write("X Coordinate: ");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Y Coordinate: ");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        liveCells.Add(new CoordSet(x, y));
+                        Console.Write("More? (Y/N) ");
+                        moreCoordsKeyPress = Console.ReadKey(); 
+                    } while (moreCoordsKeyPress.Key == ConsoleKey.Y);
+                    
                     break;
                 default:
                     break;
             }
+            #endregion
+
+            Automaton a = new Automaton(sizeX, sizeY, liveCells);
+
+
         }
     }
 }
