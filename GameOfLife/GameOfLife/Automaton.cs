1b4ad2d62fc722ab018059316179f121beab8f7f
Outrigger047-VEGAS <andrew.wyshak@gmail.com>
2015-12-15 22:46:33 -0500

    Added signatures for Iterate and GetNeighborState methods. Started building out getter for Universe property.

--- GameOfLife/GameOfLife/Automaton.cs
+++ GameOfLife/GameOfLife/Automaton.cs
@@ -79,16 +79,58 @@ namespace GameOfLife
         {
             get
             {
-                List<CoordSet> uOut = new List<CoordSet>();
+                List<CoordSet> universeOut = new List<CoordSet>();
 
-                // TODO Implement universe conversion to list of live cells
+                foreach(var cell in universe)
+                {
+                    if(cell.State == CellStateTypes.Alive)
+                    {
+                        universeOut.Add(cell.)
+                        /*Cannot recall how exactly to derive an index when using
+                        foreach, but regardless, how is a 2D array traversed? Right
+                        now I'm not sure how to determine where we are...*/
+                    }
+                }
 
-                return uOut;
+                return universeOut;
             }
         }
         #endregion
 
         #region Methods
+        // TODO Implement public Iterate method to advance the age of the universe by 1
+        public void Iterate()
+        {
+
+        }
+
+        // TODO Implement private GetNeighborState method to be used by Iterate to determine
+        // states of nearby cells
+        private CellStateTypes GetNeighborState(CoordSet currentPos, Cardinals target)
+        {
+            switch(target)
+            {
+                case Cardinals.N:
+                    return universe[currentPos.X, currentPos.Y - 1].State;
+                case Cardinals.NE:
+                    return universe[currentPos.X + 1, currentPos.Y - 1].State;
+                case Cardinals.E:
+                    return universe[currentPos.X + 1, currentPos.Y].State;
+                case Cardinals.SE:
+                    return universe[currentPos.X + 1, currentPos.Y + 1].State;
+                case Cardinals.S:
+                    return universe[currentPos.X, currentPos.Y + 1].State;
+                case Cardinals.SW:
+                    return universe[currentPos.X - 1, currentPos.Y + 1].State;
+                case Cardinals.W:
+                    return universe[currentPos.X - 1, currentPos.Y].State;
+                case Cardinals.NW:
+                    return universe[currentPos.X - 1, currentPos.Y - 1].State;
+                default:
+                    break;
+            }
+        }
+
         #endregion
 
 
