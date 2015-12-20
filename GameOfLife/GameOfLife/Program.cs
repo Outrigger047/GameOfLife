ecb6a5124f7fe5df4cd93ef4a270269273ae0a96
Outrigger047-HOME <andrew.wyshak@gmail.com>
2015-12-20 09:38:46 -0500

    Rewriting Main

--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,7 +1,4 @@
-﻿// Preprocessor directives
-#define DEBUG
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -13,7 +10,6 @@ namespace GameOfLife
         static void Main(string[] args)
         {
             #region Get console input
-#if !DEBUG
             Console.Write("Horiz size of universe: ");
             int sizeX = Convert.ToInt32(Console.ReadLine());
 
@@ -23,64 +19,7 @@ namespace GameOfLife
             Console.Write("Input max age: ");
             int maxAge = Convert.ToInt32(Console.ReadLine());
 
-
-            Console.WriteLine("Specify live cell coordinate input mode:");
-            Console.WriteLine("1   External text file");
-            Console.WriteLine("2   Manual list input");
-            List<CoordSet> liveCells = new List<CoordSet>();
-            ConsoleKeyInfo inputModeKeyPress = Console.ReadKey();
-            switch (inputModeKeyPress.ToString())
-            {
-                case "1":
-                    string[] fileData;
-                    Console.WriteLine("File path: ");
-
-                    try
-                    {
-                        fileData = File.ReadAllLines(Console.ReadLine());
-                        foreach (var line in fileData)
-                        {
-                            if (Regex.IsMatch(line, "^[0-9]+,[0-9]+$"))
-                            {
-                                liveCells.Add(
-                                    new CoordSet(
-                                        Convert.ToInt32(line.Split(',')[0]),
-                                        Convert.ToInt32(line.Split(',')[1])));
-                            }
-                        }
-                    }
-                    catch (IOException ioe)
-                    {
-                        Console.Write("IO Exception: ", ioe.Message);
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-
-                    break;
-                case "2":
-                    ConsoleKeyInfo moreCoordsKeyPress;
-                    do
-                    {
-                        Console.Write("X Coordinate: ");
-                        int x = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("Y Coordinate: ");
-                        int y = Convert.ToInt32(Console.ReadLine());
-                        liveCells.Add(new CoordSet(x, y));
-                        Console.Write("More? (Y/N) ");
-                        moreCoordsKeyPress = Console.ReadKey();
-                    } while (moreCoordsKeyPress.Key == ConsoleKey.Y);
-
-                    break;
-                default:
-                    break;
-            } 
-#endif
             #endregion
-
-#if DEBUG
-            int sizeX = 25, sizeY = 25;
 
             List<CoordSet> liveCells = new List<CoordSet>
             {
@@ -102,8 +41,6 @@ namespace GameOfLife
                 new CoordSet(7, 7),
                 new CoordSet(7, 8)
             };
-
-#endif
 
             Automaton a = new Automaton(sizeX, sizeY, liveCells);
 
