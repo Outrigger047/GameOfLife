23117c78107c4fc22a807563e03a90c9209ae0cb
Outrigger047-WORK <andrew.wyshak@gmail.com>
2015-12-20 20:51:49 -0500

    GUI setup

--- GameOfLife/WinFormsGameOfLife/Form1.Designer.cs
+++ GameOfLife/WinFormsGameOfLife/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsGameOfLife
 {
-    partial class Form1
+    partial class SizeInputForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.xLabel = new System.Windows.Forms.Label();
+            this.xUpDown = new System.Windows.Forms.NumericUpDown();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.yUpDown = new System.Windows.Forms.NumericUpDown();
+            this.generateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yUpDown)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(60, 27);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(14, 13);
+            this.xLabel.TabIndex = 0;
+            this.xLabel.Text = "X";
+            // 
+            // xUpDown
+            // 
+            this.xUpDown.Location = new System.Drawing.Point(80, 25);
+            this.xUpDown.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.xUpDown.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.xUpDown.Name = "xUpDown";
+            this.xUpDown.Size = new System.Drawing.Size(41, 20);
+            this.xUpDown.TabIndex = 1;
+            this.xUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Location = new System.Drawing.Point(60, 61);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(14, 13);
+            this.yLabel.TabIndex = 2;
+            this.yLabel.Text = "Y";
+            // 
+            // yUpDown
+            // 
+            this.yUpDown.Location = new System.Drawing.Point(80, 59);
+            this.yUpDown.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.yUpDown.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.yUpDown.Name = "yUpDown";
+            this.yUpDown.Size = new System.Drawing.Size(41, 20);
+            this.yUpDown.TabIndex = 3;
+            this.yUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(153, 37);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 4;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            // 
+            // SizeInputForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 173);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(261, 110);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.yUpDown);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.xUpDown);
+            this.Controls.Add(this.xLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SizeInputForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Game of Life";
+            ((System.ComponentModel.ISupportInitialize)(this.xUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.NumericUpDown xUpDown;
+        private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.NumericUpDown yUpDown;
+        private System.Windows.Forms.Button generateButton;
     }
 }
 
--- GameOfLife/WinFormsGameOfLife/Form1.cs
+++ GameOfLife/WinFormsGameOfLife/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinFormsGameOfLife
 {
-    public partial class Form1 : Form
+    public partial class SizeInputForm : Form
     {
-        public Form1()
+        public SizeInputForm()
         {
             InitializeComponent();
         }
