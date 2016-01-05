696f0bf4c65c606a103b7b4e961ea1140b644c18
Outrigger047-HOME <andrew.wyshak@gmail.com>
2016-01-05 18:39:51 -0500

    Went back and put in regions

--- GameOfLife/GameOfLife.FileReader/FileReader.cs
+++ GameOfLife/GameOfLife.FileReader/FileReader.cs
@@ -4,9 +4,20 @@ using System.Collections.Generic;
 
 namespace GameOfLife
 {
+    /// <summary>
+    /// Helper class to translate file data containing initial setups and cell configurations
+    /// </summary>
     public static class FileReader
     {
-        public static List<Automaton.CoordSet> ReadRunLengthEncoding(string data)
+        #region Enums
+        public enum EncodingTypes
+        {
+            Life105, Life106, Plaintext, RLE, SOF
+        }
+        #endregion
+
+        #region Public methods
+        public static List<Automaton.CoordSet> ReadFile(byte data)
         {
             List<Automaton.CoordSet> initLiveCells = new List<Automaton.CoordSet>();
 
@@ -14,5 +25,32 @@ namespace GameOfLife
 
             return initLiveCells;
         }
+
+        public static List<Automaton.CoordSet> ReadFile(byte data, EncodingTypes readAsType)
+        {
+            List<Automaton.CoordSet> initLiveCells = new List<Automaton.CoordSet>();
+
+
+
+            return initLiveCells;
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Read Run Length Encoded file data.
+        /// Used on http://www.conwaylife.com/wiki
+        /// </summary>
+        /// <param name="data">Data from external file</param>
+        /// <returns>List of CoordSet objects indicating live cells</returns>
+        private static List<Automaton.CoordSet> ReadRunLengthEncoding(byte data)
+        {
+            List<Automaton.CoordSet> initLiveCells = new List<Automaton.CoordSet>();
+
+
+
+            return initLiveCells;
+        } 
+        #endregion
     }
 }
